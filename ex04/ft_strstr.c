3c8f436a3d3582c0198987ab44f2768f79a08621
liulm <lionelulm67@gmail.com>
2024-07-23 12:25:08 +0200

    updated c03

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: liulm <liulm@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 14:01:28 by liulm             #+#    #+#             */
-/*   Updated: 2024/07/22 16:54:49 by liulm            ###   ########.fr       */
+/*   Updated: 2024/07/23 12:24:46 by liulm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,21 @@ char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
 	int	j;
+	int	len;
 
 	i = 0;
 	j = 0;
+	len = -1;
 	while (str[i])
 	{
+		len++;
 		while (to_find[j] == str[i + j])
 		{
+			j++;
 			if (to_find[j] == '\0')
 			{
-				return (str + i);
+				return (&str[len]);
 			}
-			j++;
 		}
 		i++;
 	}
@@ -35,11 +38,14 @@ char	*ft_strstr(char *str, char *to_find)
 // ----------------------------------------------------
 
 #include <stdio.h>
+#include <string.h>
 
 int	main()
 {
 	char	str[] = "Salut comment ca va?";
-	char	to_find[] = "comment";
-
-	printf("%s", ft_strstr(str, to_find));
+	char	to_find[] = "o";
+	char	str2[] = "Salut comment ca va?";
+	char	to_find2[] = "o";
+	printf("%s\n", strstr(str, to_find));
+	printf("%s", ft_strstr(str2, to_find2));
 }
