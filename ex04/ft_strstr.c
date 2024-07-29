050de932f2bbc7e4140c6f84f3d66ac58de96d23
Shiroii || Lio <139677855+Shiiroii@users.noreply.github.com>
2024-07-29 12:32:53 +0200

    Update ft_strstr.c

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -16,14 +16,10 @@ char	*ft_strstr(char *str, char *to_find)
 	int	j;
 
 	i = 0;
-	if (to_find[0] == '\0')
-	{
-		return (str);
-	}
 	while (str[i])
 	{
 		j = 0;
-		while (to_find[j] == str[i + j])
+		while (str[i + j] == to_find[j])
 		{
 			if (to_find[j + 1] == '\0')
 			{
